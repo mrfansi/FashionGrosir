913f193be9662e2871f6dfa9a0ac053875044fab
Muhammad Irfan <funnyirfan321@gmail.com>
2018-04-15 02:08:42 +0700

    Update

--- application/admin/views/CRUD_Kategori.php
+++ application/admin/views/CRUD_Kategori.php
@@ -3,34 +3,61 @@
     var hashing = '<?= $this->security->get_csrf_hash(); ?>';
 </script>
 <br>
-<section ng-controller="CrudKategoriController">
-    <div class="col">
-        <div class="card">
-            <div class="card-header d-flex align-items-center">
-                <h4>Buat Kategori Baru</h4>
+<section class="container" ng-controller="CrudKategoriController">
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-header d-flex align-items-center">
+                    <h4>Buat Kategori Baru</h4>
+                </div>
+                <div class="card-body">
+                    <form ng-submit="katsimpan()">
+                        <div class="row">
+                            <div class="col-sm-12 form-group">
+                                <label>Kategori</label>
+                                <input type="text" class="form-control" placeholder="Nama Kategori" ng-model="kat_nama" name="kat_nama">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12 form-group">
+                                <label>Parent</label>
+                                <select class="form-control" ng-model="kat_parent_id" name="kat_parent_id">
+                                    <option value="0">Root</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12 form-group">
+                                <button class="btn btn-primary btn-block">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div class="card-body">
-                <form ng-submit="katsimpan()">
-                    <div class="row">
-                        <div class="col-sm-12 form-group">
-                            <label>Kategori</label>
-                            <input type="text" class="form-control" placeholder="Nama Kategori" ng-model="kat_nama" name="kat_nama">
-                        </div>
+        </div>
+
+        <div class="col-sm-8">
+            <div class="card">
+                <div class="card-header d-flex align-items-center">
+                    <h4>List Kategori</h4>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-sm">
+                            <thead>
+                            <tr>
+                                <th>Nama</th>
+                                <th>Parent</th>
+                                <th>Aksi</th>
+                            </tr>
+                            </thead>
+
+                            <tbody>
+
+                            </tbody>
+                        </table>
                     </div>
-                    <div class="row">
-                        <div class="col-sm-12 form-group">
-                            <label>Parent</label>
-                            <select class="form-control" ng-model="kat_parent_id" name="kat_parent_id">
-                                <option value="0">Root</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-12 form-group">
-                            <button class="btn btn-primary btn-block">Submit</button>
-                        </div>
-                    </div>
-                </form>
+                </div>
             </div>
         </div>
     </div>
