0b0510918e453a216604097504f75f1935e78bf3
Muhammad Irfan <funnyirfan321@gmail.com>
2018-04-15 01:17:51 +0700

    Update

--- application/admin/controllers/Item.php
+++ application/admin/controllers/Item.php
@@ -12,6 +12,7 @@ class Item extends MY_Controller
     {
         parent::__construct();
         $this->load->model('Ms_item', 'item');
+        $this->load->model('Ms_item_kategori', 'kat_item');
     }
 
     public function index()
@@ -21,13 +22,27 @@ class Item extends MY_Controller
 
     public function baru()
     {
+        $msg = array();
         if ($this->input->server('REQUEST_METHOD') == 'GET') {
             $this->load->view('CRUD_Item');
         } else if ($this->input->server('REQUEST_METHOD') == 'POST') {
-            return 0;
+
+            $data = array(
+                'Kat_Nama'      => $this->input->post('nama'),
+                'Kat_Parent_ID' => $this->input->post('parent')
+            );
+
+            if ($this->kat_item->insert($data))
+            {
+                $msg['status'] = true;
+            }
+            else{
+                $msg['status'] = false;
+            }
+
+            echo $msg;
         }
     }
-
 
     public function kategori($kat)
     {
---
+++ application/admin/models/Ms_item_kategori.php
@@ -0,0 +1,7 @@
+<?php
+
+class Ms_item_kategori extends MY_Model
+{
+    protected $_table = 'ms_Item_Kategori';
+    protected $primary_key = 'Kat_ID';
+}
