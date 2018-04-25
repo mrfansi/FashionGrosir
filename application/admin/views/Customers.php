ac326e4a7e18089b8096abe55c08af19ccc065f5
Muhammad Irfan <funnyirfan321@gmail.com>
2018-04-25 19:29:57 +0700

    Update

--- application/admin/views/Customers.php
+++ application/admin/views/Customers.php
@@ -1,14 +1,123 @@
-<br>
-<section ng-controller="CustomersController">
-    <div class="col">
-        <div class="card">
-            <div class="card-header d-flex align-items-center">
-                <h2>Customers</h2>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?= $title; ?></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
+    <!-- Bootstrap CSS-->
+    <link rel="stylesheet" href="<?= base_url('assets/vendor/bootstrap/css/bootstrap.min.css'); ?>">
+    <!-- Font Awesome CSS-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+    <!-- Fontastic Custom icon font-->
+    <link rel="stylesheet" href="<?= base_url('assets/css/fontastic.css'); ?>">
+    <!-- Google fonts - Roboto -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700">
+    <!-- jQuery Circle-->
+    <link rel="stylesheet" href="<?= base_url('assets/css/grasp_mobile_progress_circle-1.0.0.min.css'); ?>">
+    <!-- Custom Scrollbar-->
+    <link rel="stylesheet"
+          href="<?= base_url('assets/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css'); ?>">
+    <!-- theme stylesheet-->
+    <link rel="stylesheet" href="<?= base_url('assets/css/style.default.css" id="theme-stylesheet'); ?>">
+    <!-- Custom stylesheet - for your changes-->
+    <link rel="stylesheet" href="<?= base_url('assets/css/custom.css'); ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/vendor/rzslider/rzslider.min.css'); ?>">
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="<?= base_url('assets/img/favicon.ico'); ?>">
+    <script>
+        var base_url = '<?= base_url(); ?>';
+        var hashing = '<?= $this->security->get_csrf_hash(); ?>';
+    </script>
+    <script src="<?= base_url('assets/vendor/rzslider/rzslider.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/js/app.js'); ?>"></script>
+</head>
+<body>
+<?php include_once('master/Menu.php'); ?>
+<div class="page">
+    <!-- navbar-->
+    <header class="header">
+        <nav class="navbar">
+            <div class="container-fluid">
+                <div class="navbar-holder d-flex align-items-center justify-content-between">
+                    <div class="navbar-header"><a id="toggle-btn" href="#" class="menu-btn"><i
+                                    class="icon-bars"> </i></a><a href="<?= base_url('adm.php/dashboard') ?>"
+                                                                  class="navbar-brand">
+                            <div class="brand-text d-none d-md-inline-block"><strong
+                                        class="text-primary">FASHION GROSIR</strong></div>
+                        </a></div>
+                    <ul class="nav-menu list-unstyled d-flex flex-md-row align-items-md-center">
+                        <li class="nav-item"><a href="<?= base_url('adm.php/auth/logout') ?>" class="nav-link logout">Logout<i
+                                        class="fa fa-sign-out"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <br>
+    <section>
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    <h2>Customers</h2>
+                </div>
+                <div class="card-body">
+                    <table class="table table-sm table-hover">
+                        <thead>
+                        <tr>
+                            <th scope="col">Username</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">IP Address</th>
+                            <th scope="col"></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach ($customers as $customer): ?>
+                        <tr>
+                            <td><?= $customer->customers_username; ?></td>
+                            <td><?= $customer->customers_email; ?></td>
+                            <td><?= $customer->customers_ipaddr; ?></td>
+                            <td>
+                                <a href="<?= site_url('customers/detil/') . $customer->customers_id; ?>" class="btn btn-sm btn-primary"></i> Detil</a>
+                                <a href="<?= site_url('customers/ubah/') . $customer->customers_id; ?>" class="btn btn-sm btn-warning"></i> Ubah</a>
+                                <a href="<?= site_url('customers/hapus') . $customer->customers_id; ?>" class="btn btn-sm btn-danger"></i> Hapus</a>
+                                <a href="<?= site_url('customers/ubah_password/') . $customer->customers_id; ?>" class="btn btn-sm btn-warning"></i> Ganti Password</a>
+                            </td>
+                        </tr>
+                        <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
 
-            <div class="card-body">
+        </div>
+    </section>
+    <footer class="main-footer">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-6">
+                    <p>Fashion Grosir &copy; 2018</p>
+                </div>
 
             </div>
         </div>
-    </div>
-</section>
+    </footer>
+</div>
+<!-- Javascript files-->
+<script src="<?= base_url('assets/vendor/jquery/jquery.min.js'); ?>"></script>
+<script src="<?= base_url('assets/vendor/popper.js/umd/popper.min.js'); ?>"></script>
+<script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.min.js'); ?>"></script>
+<script src="<?= base_url('assets/js/grasp_mobile_progress_circle-1.0.0.min.js'); ?>"></script>
+<script src="<?= base_url('assets/vendor/jquery.cookie/jquery.cookie.js'); ?>"></script>
+<script src="<?= base_url('assets/vendor/chart.js/Chart.min.js'); ?>"></script>
+<script src="<?= base_url('assets/vendor/jquery-validation/jquery.validate.min.js'); ?>"></script>
+<script src="<?= base_url('assets/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js'); ?>"></script>
+<script src="<?= base_url('assets/vendor/loadingoverlay/loadingoverlay.min.js'); ?>"></script>
+<script src="<?= base_url('assets/vendor/loadingoverlay/loadingoverlay_progress.min.js'); ?>"></script>
+<!-- Main File-->
+<script src="<?= base_url('assets/js/front.js'); ?>"></script>
+</body>
+</html>
