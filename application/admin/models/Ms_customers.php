6393e7a694d0176358baba4bf822677edd21a839
Muhammad Irfan <funnyirfan321@gmail.com>
2018-04-25 19:24:32 +0700

    Update

---
+++ application/admin/models/Ms_customers.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: irfandihati
+ * Date: 24/02/2018
+ * Time: 18.01
+ */
+
+class Ms_customers extends MY_Model {
+
+    public $table = 'ms_customers'; // you MUST mention the table name
+    public $primary_key = 'customers_id'; // you MUST mention the primary key
+    public $fillable = array(); // If you want, you can set an array with the fields that can be filled by insert/update
+    public $protected = array(); // ...Or you can set an array with the fields that cannot be filled by insert/update
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
