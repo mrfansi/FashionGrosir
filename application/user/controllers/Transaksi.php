036ab1369821064775120419422752ae7d65c623
Muhammad Irfan <funnyirfan321@gmail.com>
2018-05-23 19:59:22 +0700

    Update

--- application/user/controllers/Transaksi.php
+++ application/user/controllers/Transaksi.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Transaksi extends MY_User
+class Transaksi extends MY_Controller
 {
     public function __construct()
     {
