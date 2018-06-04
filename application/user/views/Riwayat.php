c653ab4ca842f47b3f05ab92ed31c7c55d7ddc47
Muhammad Irfan <funnyirfan321@gmail.com>
2018-06-04 21:02:22 +0700

    Update Layout

--- application/user/views/Riwayat.php
+++ application/user/views/Riwayat.php
@@ -34,7 +34,7 @@ include "layout/Menu.php";
                     <a class="list-group-item list-group-item-action r-active-step" href="<?= site_url('riwayat'); ?>">Riwayat
                         Pesanan</a>
                     <a class="list-group-item list-group-item-action" href="<?= site_url('pending'); ?>">Transaksi
-                        Pending</a>
+                        Tertunda</a>
                 </div>
             </div>
 
