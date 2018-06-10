8bfc576e5799dc9ad974d731eefc831ebdb5d569
Muhammad Irfan <funnyirfan321@gmail.com>
2018-06-11 04:36:32 +0700

    Update

--- application/user/views/Login.php
+++ application/user/views/Login.php
@@ -4,18 +4,37 @@ include "layout/Header.php";
 <?php if (isset($log) && $log != ""): ?>
     <p class="text-danger text-center"><?= $log; ?></p>
 <?php endif; ?>
+
     <!-- Login -->
     <form method="post" action="<?= site_url('login'); ?>" class="form-signin">
+        <?php if (isset($_SESSION['berhasil']) && $_SESSION['berhasil'] != ""): ?>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <?php echo $_SESSION['berhasil']; ?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        <?php endif; ?>
+        <?php if (isset($_SESSION['gagal']) && $_SESSION['gagal'] != ""): ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <?php echo $_SESSION['gagal']; ?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        <?php endif; ?>
         <input type="hidden" name="token_fg" value="<?= $this->security->get_csrf_hash(); ?>">
         <h4 class="h4 mb-3 font-weight-normal text-center f-title-color"><?= $brandname; ?></h4>
         <div class="form-group">
             <label for="email" class="sr-only">Email</label>
-            <input type="email" id="email" name="email" class="form-control f-input-shadow" placeholder="Email address" required
+            <input type="email" id="email" name="email" class="form-control f-input-shadow" placeholder="Email address"
+                   required
                    autofocus autocomplete="off">
         </div>
         <div class="form-group">
             <label for="password" class="sr-only">Password</label>
-            <input type="password" id="password" name="password" class="form-control f-input-shadow" placeholder="Password"
+            <input type="password" id="password" name="password" class="form-control f-input-shadow"
+                   placeholder="Password"
                    required autocomplete="off">
         </div>
 
--- application/user/views/Register.php
+++ application/user/views/Register.php
@@ -4,17 +4,24 @@ include "layout/Header.php";
 <?php if (isset($log) && $log != ""): ?>
     <p class="text-danger text-center"><?= $log; ?></p>
 <?php endif; ?>
-<?php if (isset($_SESSION['berhasil']) && $_SESSION['berhasil'] != ""): ?>
-    <div class="col">
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            <?php echo $_SESSION['berhasil']; ?>
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-    </div>
-<?php endif; ?>
+
     <form class="form-signin" action="<?= site_url('auth/register_post'); ?>" method="post">
+        <?php if (isset($_SESSION['berhasil']) && $_SESSION['berhasil'] != ""): ?>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <?php echo $_SESSION['berhasil']; ?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        <?php endif; ?>
+        <?php if (isset($_SESSION['gagal']) && $_SESSION['gagal'] != ""): ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <?php echo $_SESSION['gagal']; ?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        <?php endif; ?>
         <input type="hidden" name="token_fg" value="<?= $this->security->get_csrf_hash(); ?>">
         <h4 class="h4 mb-3 font-weight-normal text-center f-title-color"><?= $brandname; ?></h4>
 
@@ -31,6 +38,11 @@ include "layout/Header.php";
         <div class="form-group">
             <label for="notelp" class="sr-only">Nomor Telp</label>
             <input type="text" id="notelp" name="notelp" class="form-control" placeholder="Nomor Telepon" required>
+        </div>
+
+        <div class="form-group">
+            <label for="password" class="sr-only">Password</label>
+            <input type="password" id="password" name="password" class="form-control" minlength="8" maxlength="15" placeholder="Password" required>
         </div>
 
         <div class="checkbox mb-3">
