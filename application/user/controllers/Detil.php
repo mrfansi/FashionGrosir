6c47ccf47af46ed0c2088a0a63809fb2e1140524
Muhammad Irfan <funnyirfan321@gmail.com>
2018-06-24 15:17:07 +0700

    Update Detil Auth

--- application/user/controllers/Detil.php
+++ application/user/controllers/Detil.php
@@ -6,6 +6,9 @@ class Detil extends MY_Controller
     public function __construct()
     {
         parent::__construct();
+        if (!$this->session->isonline) {
+            redirect('login');
+        }
     }
 
     public function index()
