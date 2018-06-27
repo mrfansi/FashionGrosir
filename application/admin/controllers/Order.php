a39198b717dc3625678bead2af3251c7c26c7c5f
Muhammad Irfan <funnyirfan321@gmail.com>
2018-06-27 12:58:07 +0700

    Update

--- application/admin/controllers/Order.php
+++ application/admin/controllers/Order.php
@@ -256,8 +256,94 @@ class Order extends MY_Controller
             }
         }
 
+    }
 
 
+    public function detil($id)
+    {
+        $this->data->orders_noid = $id;
+        $this->data->orders = $this->order->with_order_detil()->where_orders_noid($this->data->orders_noid)->get();
+        $this->data->orders_total = function () {
+            $hasil = 0;
+            foreach ($this->data->orders->order_detil as $order) {
+                $hasil += $order->orders_detil_tharga;
+            }
+            return $hasil;
+        };
+        $this->data->pengiriman = function () {
+            $alamat = new stdClass();
+            $order_pengiriman = $this->order_pengiriman->where('orders_noid', $this->data->orders_noid)->get();
+
+            if ($order_pengiriman) {
+                $alamat->provinsi = $this->provinsi
+                    ->where('provinsi_id', $order_pengiriman->orders_pengiriman_provinsi)
+                    ->get()->provinsi_nama;
+                $alamat->kabupaten = $this->kabupaten
+                    ->where('kabupaten_id', $order_pengiriman->orders_pengiriman_kabupaten)
+                    ->get()->kabupaten_nama;
+                $alamat->kecamatan = $this->kecamatan
+                    ->where('kecamatan_id', $order_pengiriman->orders_pengiriman_kecamatan)
+                    ->get()->kecamatan_nama;
+                $alamat->desa = $this->desa
+                    ->where('desa_id', $order_pengiriman->orders_pengiriman_desa)
+                    ->get()->desa_nama;
+                $hasil = $order_pengiriman->orders_pengiriman_deskripsi . '<br>' . $alamat->desa . '<br>' . $alamat->kecamatan . ', ' . $alamat->kabupaten . '<br>' .
+                    $alamat->provinsi . ', ' . $order_pengiriman->orders_pengiriman_kodepos;
+            } else {
+                $hasil = '';
+            }
+            return $hasil;
+
+        };
+
+        $this->data->jasa = function () {
+            $ongkir = $this->order_ongkir->where('orders_noid', $this->data->orders_noid)->get();
+
+            if ($ongkir) {
+                return $ongkir->orders_ongkir_nama . ' - ' . $ongkir->orders_ongkir_deskripsi . ' (' . $ongkir->orders_ongkir_estimasi . ' hari)';
+
+            } else {
+                return 'Belum menentukan metode pengiriman';
+            }
+
+        };
+
+        $this->data->metode_pembayaran = function () {
+            $orders_noid = $this->order
+                ->where('orders_noid', $this->data->orders_noid)
+                ->get()->orders_noid;
+            $pembayaran = $this->order_payment->with_bank()->where('orders_noid', $orders_noid)->get();
+
+            if ($pembayaran) {
+                $hasil = $pembayaran->bank->bank_penerbit . ' - (A/N: ' . $pembayaran->bank->bank_nama . ') (Nomor Rek: ' . $pembayaran->bank->bank_rek . ')';
+
+            } else {
+                $hasil = 'Belum menentukan metode pembayaran';
+            }
+
+            return $hasil;
+        };
+
+        $this->data->biaya_subtotal = function () {
+            $hasil = 0;
+            $orders_noid = $this->order
+                ->where('orders_noid', $this->data->orders_noid)
+                ->get()->orders_noid;
+            foreach ($this->order_detil->where('orders_noid', $orders_noid)->get_all() as $od) {
+                $hasil += (int)$od->orders_detil_tharga;
+            }
+
+            return $hasil;
+        };
+
+        $this->data->biaya_pengiriman = function () {
+            $orders_noid = $this->order
+                ->where('orders_noid', $this->data->orders_noid)
+                ->get()->orders_noid;
+            $ongkir = $this->order_ongkir->where('orders_noid', $orders_noid)->get();
+            return (int)$ongkir->orders_ongkir_biaya;
+        };
+        $this->load->view('Detil_order', $this->data);
     }
 
 }
